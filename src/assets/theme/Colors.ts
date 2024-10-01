b28c12373ae3bd0d97c603223c30bd1920fb0030
cihan base <cihanbas07@gmail.com>
2024-10-01 12:52:01 +0300

    updated colors

--- src/assets/theme/Colors.ts
+++ src/assets/theme/Colors.ts
@@ -1,10 +1,16 @@
 const colors = {
   white: '#fff',
-  primary: '#FF3D00',
+  primary: '#E13287',
   background: '#131313',
   card: '#9DB2CE',
-  text: '#9DB2CE',
+  text: '#fff',
   border: '#9DB2CE',
   notification: 'rgb(255, 69, 58)',
+  transparent: 'rgba(0,0,0,0.5)',
+  chipBG: '#FFF3EB',
+  chipTextColor: '#FF700F',
+  inputBG: '#060433',
+  placeholder: '#616e7e',
+  black: '#000',
 };
 export { colors };
